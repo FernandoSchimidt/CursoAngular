7aaa4ec630b2c43dcd7a48fdbc0858d31bb699c5
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-18 23:47:20 -0300

    fim Calculadora

--- projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -18,4 +18,26 @@ describe('CalculadoraService', () => {
       let soma = service.calcular(1, 4, CalculadoraService.SOMA);
       expect(soma).toEqual(5);
     }))
+
+  it('dev garantir que 1- 4 =-3',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let subtracao = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-3);
+    }))
+  it('dev garantir que 1/ 4 =0.25',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let divisao = service.calcular(1, 4, CalculadoraService.DIVISAO);
+      expect(divisao).toEqual(0.25);
+    }))
+  it('dev garantir que 1* 4 =4',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let multi = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect(multi).toEqual(4);
+
+    }))
+    it('Operacao invalida retorna zero 0',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let operacaoInvalida = service.calcular(1, 4, '%');
+      expect(operacaoInvalida).toEqual(0);
+    }))
 });
