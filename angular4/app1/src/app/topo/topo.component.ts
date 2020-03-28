c3448f5fa3512245e0903871897487e711bf763d
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-28 20:34:08 -0300

    Angular 4

---
+++ angular4/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'app-topo',
+    templateUrl:'./topo.component.html',
+    //template:'<p>Fernando</p>',
+    styleUrls:['./topo.component.css']
+})
+export class TopoComponent {
+    public titulo:string = 'Aprendendo Inglês'
+}
