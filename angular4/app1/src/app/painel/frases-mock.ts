d152a92b79cf1f334182aa083feebf05e59df669
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-29 12:01:15 -0300

    angular 4

--- angular4/app1/src/app/painel/frases-mock.ts
+++ angular4/app1/src/app/painel/frases-mock.ts
@@ -2,7 +2,7 @@ import { Frase } from '../shared/frase.model'
 
 export const FRASES: Frase[] = [
     { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
-    { fraseEng: 'I wach tv', frasePtBr: 'Eu Assito Tv' },
+    { fraseEng: 'I wach tv', frasePtBr: 'Eu assito tv' },
     { fraseEng: 'How are you?', frasePtBr: 'Como vai você' },
     { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão' }
 ]
--- angular4/app1/src/app/painel/painel.component.ts
+++ angular4/app1/src/app/painel/painel.component.ts
@@ -12,13 +12,48 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase'
-  public resposta: string
-  constructor() { }
+  public resposta: string = ''
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor() {
+    this.atualizaRodada()
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit(): void {
   }
   public atualizaResposta(resposta: Event): void {
-    this.resposta =((<HTMLInputElement>resposta.target).value)
-    console.log(this.resposta)
+    this.resposta = ((<HTMLInputElement>resposta.target).value)
+    // console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+
+      alert('A tradução está correta!')
+      //Trocar pergunta da rodada
+      this.rodada++
+
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      // console.log(this.progresso)
+
+      // console.log(this.rodada)
+      this.atualizaRodada()
+      //limpar a resposta
+      this.resposta = ''
+    } else {
+      alert('A tradução está errada!')
+
+    }
+
+  }
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+
   }
 }
