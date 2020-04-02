5112aa58c66bf73c0c359308099c77c7a25bddda
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-04-02 18:47:57 -0300

    passaro urbano

--- angular4/app2/src/app/ofertas.service.ts
+++ angular4/app2/src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
+import { getLocaleDayPeriods } from '@angular/common'
 
 @Injectable()
 export class OfertasService {
@@ -11,9 +12,18 @@ export class OfertasService {
         //Efetuar uma requisiçaõ http
         return this.http.get('http://localhost:3000/ofertas?destaque=true')
             .toPromise()
-            .then((resposta:any)=>resposta)
+            .then((resposta: any) => resposta)
         //retornar um promise
 
     }
 
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta: any) => resposta)
+    }
+    
+
 }
+
+// json-server --watch banco-de-dados.json
