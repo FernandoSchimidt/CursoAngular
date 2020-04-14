5eba2a18dfaad6763a683a9c5469c340d3a9906b
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-04-13 21:29:57 -0300

    s

--- angular4/app2/src/app/topo/topo.component.ts
+++ angular4/app2/src/app/topo/topo.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service'
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import { debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators'
+import { of } from 'rxjs';
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
@@ -11,19 +14,36 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
+  private subjectPesquisa: Subject<string> = new Subject
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(), //busca se o termo for diferente do anterior
+      switchMap((termo: string) => {
+        console.log('requisição http para a api ', termo)
+
+        if (termo.trim() === '') {
+          //retornar um observable de array de ofertas vazio
+          return of([])
+        }
+        return this.ofertasService.pesquisaOfertas(termo)
+      }),
+      catchError((erro) => {
+        console.log(erro)
+        return of([])
+      })
+    )
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      this.ofertas2 = ofertas
+    })
   }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status', erro.status),
-      ()=>console.log('Fluxo de eventos completo')
-    )
+    this.subjectPesquisa.next(termoDaBusca)
   }
 
 }
