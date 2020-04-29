931297979f99ce0d34a01bc4be6a091b8d2f5fd3
=fernando schimidt <=>
2020-04-29 14:56:25 -0300

    angular
    s

---
+++ lifecycle/src/app/main-lifecycle/life-cycle-child/life-cycle-child.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
+export interface LifeCicleEvent {
+  id: number;
+  name: string;
+  color: string;
+}
+@Component({
+  selector: 'app-life-cycle-child',
+  templateUrl: './life-cycle-child.component.html',
+  styleUrls: ['./life-cycle-child.component.css']
+})
+export class LifeCycleChildComponent implements OnInit, OnDestroy, OnChanges {
+
+  @Input() name: string;
+  @Input() age: number;
+  @Input() food: string;
+
+  public events: LifeCicleEvent[] = [];
+  nextEventId: number = 0;
+
+  colors: string[] = ["accent", "warn", "primary"]
+
+  constructor() {
+    console.log(this.name + " - constructor")
+    this.newEvent("constructor");
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.name + " - ngOnInit")
+    this.newEvent("ngOnInit");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.name + " - ngOnChanges")
+    this.newEvent("ngOnChanges");
+    for (let propName in changes) {
+      console.log(propName)
+    }
+    // if (changes['name']) {
+    //   console.log('new name' + changes['name'].currentValue);
+    // }
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log(this.name + " - After")
+    this.newEvent("ngAfterContentInit");
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log(this.name + " - ngAfterInit")
+    this.newEvent("ngAfterViewInit");
+  }
+  ngOnDestroy(): void {
+    console.log(this.name + " - ngOnDestroy")
+    this.newEvent("ngOnDestroy");
+  }
+  newEvent(name: string) {
+    let i = this.nextEventId++;
+    this.events.push({ id: i, color: this.colors[i % this.colors.length], name: name });
+    setTimeout(() => {
+      let idx = this.events.findIndex((e) => e.id = i);
+      if (idx >= 0) {
+        this.events.splice(idx, 1)
+      }
+    }, 3000 + this.events.length * 2000)
+  }
+
+}
---
+++ lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+
+@Component({
+  selector: 'app-main-lifecycle',
+  templateUrl: './main-lifecycle.component.html',
+  styleUrls: ['./main-lifecycle.component.css']
+})
+export class MainLifecycleComponent implements OnInit {
+  clients: Client[] = [];
+  foods: string[] = ["Rice", "Beans", "Meat", "Pizza", "Burger"];
+  name: string;
+  age: number;
+  food: string;
+  editClient: number = -1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  save() {
+    if (this.editClient == -1) {
+
+      this.clients.push({
+        name: this.name,
+        age: this.age,
+        food: this.food
+      })
+    } else {
+      this.clients[this.editClient].age = this.age;
+      this.clients[this.editClient].name = this.name;
+      this.clients[this.editClient].food = this.food;
+      this.editClient = -1;
+    }
+    this.limpaCampos()
+  }
+  limpaCampos() {
+    this.name = ""
+    this.age = null
+    this.food = ""
+  }
+  remove(i: number) {
+    this.clients.splice(i, 1)
+  }
+  edit(i: number) {
+    this.age = this.clients[i].age;
+    this.name = this.clients[i].name;
+    this.food = this.clients[i].food;
+    this.editClient = i
+  }
+}
